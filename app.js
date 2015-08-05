c2f2f9266b981a315be763b4c4215cd9973cb940
Lowry <Lowrytang@163.com>
2015-08-05 14:03:16 +0800

    add meta

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var favicon = require('serve-favicon');
 var app = express();
 
 app.set('port', (process.env.PORT || 3000));
 
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), function() {
